647e33ee138f30c5364433e698710a57b9d4770f
Kris Steigerwald <kris.steigerwald@gmail.com>
2022-10-22 02:33:35 -0600

    mock json

--- HangTime/Model/Friend.swift
+++ HangTime/Model/Friend.swift
@@ -7,8 +7,25 @@
 
 import Foundation
 
-struct Friend: Codable, Identifiable {
-    let id: Int
-    let image: String
+struct Location: Codable {
+    var lat: Double
+    var long: Double
+}
+
+struct Profile: Codable {
     let firstName: String
+    let lastName: String
+    let dob: String
+    let image: String
+    let about: String
+    var location: Location
+}
+
+struct Friend: Codable, Identifiable {
+    let id: String
+    let email: String
+    let username: String
+    let profile: Profile
+    let createdAt: String
+    let updatedAt: String
 }
